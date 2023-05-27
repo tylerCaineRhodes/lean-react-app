dd05dd30437fe83e9a7c64d3cda237691cedf07c
Tyler Rhodes <rhodetyl000@gmail.com>
2023-05-26 23:32:25 -0700

    Add extra boiler for beginning styles

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,9 +5,12 @@ import '../tailwind.css';
 export default function App() {
   return (
     <React.Fragment>
-      <div className={`${style.app} flex flex-col justify-center items-center`}>
+      <div
+        className={`${style.app} flex flex-col justify-center items-center h-screen`}
+      >
         <h1>React App</h1>
         <p>React App Rendered!</p>
+        <p className="custom-banner">Time to build an app....</p>
       </div>
     </React.Fragment>
   );
